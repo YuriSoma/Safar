11bc15541856151a54abedc71a637b5e99bd9140
YuriSoma <yaser.alelayan@outlook.com>
2022-12-17 15:08:44 +0300

    Update index file

---
+++ References/Learn JS/script.js
@@ -0,0 +1,175 @@
+var ys = "Marhaba";
+var test = function (){
+	var hiVar = "Hi from JS to DIV inside HTML";
+	return hiVar;
+}
+////** Sum test ///
+var sum = 0, i;
+for (i = 0; i < 10; i++){
+	console.log(i);
+	sum += i;
+}
+console.log("Now SUM is: " + sum);
+console.log("And i is: " + i);
+
+
+// Functions are First-Class Data Types
+// Functions ARE objects
+function multiply(x, y) {
+  return x * y;
+}
+multiply.version = "v.1.0.0";
+console.log(multiply.version);
+
+
+// Function factory
+function makeMultiplier(multiplier) {
+  var myFunc = function (x) {
+    return multiplier * x;
+  };
+
+  return myFunc;
+}
+
+var multiplyBy3 = makeMultiplier(3);
+console.log(multiplyBy3(10));
+var doubleAll = makeMultiplier(2);
+console.log(doubleAll(100));
+
+
+
+// Passing functions as arguments
+function doOperationOn(x, operation) {
+  return operation(x);
+}
+
+var result = doOperationOn(5, multiplyBy3);
+console.log(result);
+result = doOperationOn(100, doubleAll);
+console.log(result);
+
+// Function constructors
+function Circle (radius) {
+  this.radius = radius;
+}
+
+Circle.prototype.getArea = 
+  function () {
+    return Math.PI * Math.pow(this.radius, 2);
+  };
+
+
+var myCircle = new Circle(10);
+console.log(myCircle.getArea());
+
+var myOtherCircle = new Circle(20);
+console.log(myOtherCircle);
+
+// Object literals and "this"
+var literalCircle = {
+  radius: 10,
+
+  getArea: function () {
+    var self = this;
+    console.log(this);
+    var increaseRadius = function () {
+      self.radius = 20;
+    };
+    increaseRadius();
+    console.log(this.radius);
+
+    return Math.PI * Math.pow(this.radius, 2);
+  }
+};
+
+console.log(literalCircle.getArea());
+
+// Arrays
+var array = new Array();
+array[0] = "Yaakov";
+array[1] = 2;
+array[2] = function (name) {
+  console.log("Hello " + name);
+};
+array[3] = {course: " HTML, CSS & JS"};
+
+console.log(array);
+array[2](array[0]);
+console.log(array[3].course);
+
+
+// Short hand array creation
+var names = ["Yaakov", "John", "Joe"];
+console.log(names);
+
+for (var i = 0; i < names.length; i++) {
+  console.log("Hello " + names[i]);
+}
+
+names[100] = "Jim";
+for (var i = 0; i < names.length; i++) {
+  console.log("Hello " + names[i]);
+}
+
+var names2 = ["Yaakov", "John", "Joe"];
+
+var myObj = {
+  name: "Yaakov",
+  course: "HTML/CSS/JS",
+  platform: "Courera"
+};
+for (var prop in myObj) {
+  console.log(prop + ": " + myObj[prop]);
+}
+
+for (var name in names2) {
+  console.log("Hello " + names2[name]);
+}
+
+names2.greeting = "Hi!";
+
+for (var name in names2) {
+  console.log("Hello " + names2[name]);
+}
+
+// Closures
+function makeMultiplier (multiplier) {
+  // var multiplier = 2;
+  function b() {
+    console.log("Multiplier is: " + multiplier);
+  }
+  b();
+
+
+  return (
+      function (x) {
+        return multiplier * x;
+      }
+
+    );
+}
+
+var doubleAll = makeMultiplier(2);
+console.log(doubleAll(10)); // its own exec env
+
+// Immediately Invoked Function Expression
+// IIFE
+(function (name) {
+  console.log("Hello " + name);
+})("Coursera!");
+
+////// IIFE Example
+(function (window) {
+  var yaakovGreeter = {};
+  yaakovGreeter.name = "Yaakov";
+  var greeting = "Hello ";
+  yaakovGreeter.sayHello = function () {
+    console.log(greeting + yaakovGreeter.name);
+  }
+
+  window.yaakovGreeter = yaakovGreeter;
+
+})(window);
+
+yaakovGreeter.sayHello(); // could be in another JS file
+
